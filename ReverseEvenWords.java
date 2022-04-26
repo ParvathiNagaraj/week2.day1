de21440be77896994eed98f4787f155eb4c08b41
ParvathiNagaraj <103828406+ParvathiNagaraj@users.noreply.github.com>
2022-04-26 14:39:43 +0530

    Update ReverseEvenWords.java

--- ReverseEvenWords.java
+++ ReverseEvenWords.java
@@ -4,15 +4,15 @@ public class ReverseEvenWords {
 
 	public static void main(String[] args) {
 
-		String test = "Iam a Software tester";
-		
-		//assigning string to string array
+		String test = "I am a Software tester";
+
+		// assigning string to string array
 		String[] str = test.split(" ");
-		
-		//iterating characters using for loop
+
+		// iterating characters using for loop
 		for (int i = 0; i < str.length; i++) {
 
-			if (i % 2 == 1) { //checking it is even word using Modulous operator
+			if (i % 2 == 0) { // checking it is even position word using Modulous operator
 
 				StringBuilder sb = new StringBuilder(str[i]);
 				str[i] = sb.reverse().toString();
